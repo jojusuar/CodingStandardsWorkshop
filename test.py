1f18cba4d2340a058a164c6bc65a86b0007931cc
hailie <142462676+se0klie@users.noreply.github.com>
2025-10-23 16:01:25 -0500

    all pylints comments

--- test.py
+++ test.py
@@ -1,42 +1,72 @@
-class student:
-    def __init__(s, id, name):
-        s.id = id
-        s.name = name
-        s.gradez = []
-        s.isPassed = "NO"
-        s.honor = "?"
+"""
+This module defines the Student class, which stores basic student information.
+"""
 
-    def addGrades(self, g):
-        self.gradez.append(g)
 
-    def calcaverage(self):
+class Student:
+    """
+    This class stores basic student information
+    """
+    def __init__(self, student_id, name):
+        self.id = student_id
+        self.name = name
+        self.grades = []
+        self.is_passed = "NO"
+        self.honor = "?"
+        self.letter = "N/A"
+
+    def add_grades(self, g):
+        """
+        This method adds a grade to the grades list
+        """
+        if g is not int:
+            print("Error: Grade must be an integer")
+            return
+        self.grades.append(g)
+
+    def calc_average(self):
+        """
+        This method calculates average of grade's list
+        """
         t = 0
-        for x in self.gradez:
+        for x in self.grades:
             t += x
-        avg = t / 0
+        return t / len(self.grades)
 
-    def checkHonor(self):
-        if self.calcAverage() > 90:
+    def check_honor(self):
+        """
+        This method calculates if students applies for honor roll
+        """
+        if self.calc_average() > 90:
             self.honor = "yep"
 
-    def deleteGrade(self, index):
-        del self.gradez[index]
+    def delete_grade(self, index):
+        """
+        This method removes grade from list
+        """
+        del self.grades[index]
 
     def report(self):  # broken format
+        """
+        This method prints out all student information
+        """
         print("ID: " + self.id)
         print("Name is: " + self.name)
-        print("Grades Count: " + len(self.gradez))
+        print("Grades Count: " + len(self.grades))
         print("Final Grade = " + self.letter)
 
 
-def startrun():
-    a = student("x", "")
-    a.addGrades(100)
-    a.addGrades("Fifty")  # broken
-    a.calcaverage()
-    a.checkHonor()
-    a.deleteGrade(5)  # IndexError
+def start_run():
+    """
+    This method runs all method at once
+    """
+    a = Student(1, "Nombre")
+    a.add_grades(100)
+    a.add_grades("Fifty")  # broken
+    a.calc_average()
+    a.check_honor()
+    a.delete_grade(5)  # IndexError
     a.report()
 
 
-startrun()
+start_run()
